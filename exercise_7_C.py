05cbe6077f1001e596e4b7b7fb0314df4e2c03f8
Muremwa <danmburu254@gmail.com>
2020-02-21 00:24:28 +0300

    added exercise 7

---
+++ exercise_7_C.py
@@ -0,0 +1,31 @@
+# accepts a filesystem path(default) or a url(indicated by True as second argument)
+# returns the longest word(here word is defined as one or more consecutive sequence of alphabets of either case)
+# assume that input encoding is utf-8 and small enough to fit in memory and that there's only one distinct longest word
+import string
+
+
+def longest_word(*args):
+    path = args[0]
+    try:
+        url = args[1]
+    except IndexError:
+        pass
+
+    words = None
+    with open(path, 'r') as f:
+        words = f.read()
+
+    words = words.split("\n")
+    # remove all punctuation
+    words = [word.translate(str.maketrans('', '', string.punctuation)) for word in words]
+    words = [word.split(" ") for word in words]
+    x = []
+    for word_list in words:
+        x = x + word_list
+
+    return sorted(x, key=len, reverse=True)[0]
+
+
+ip_path = 'files/poem.txt'
+print(longest_word(ip_path))
+# 'Violets'
